aa7234497636912a2440a6617643f3e36073902f
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-26 23:04:24 -0500

    Creada las vistas de la entidad Persona, listado

--- web/views.py
+++ web/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Persona
+from .serializers import PersonaSerializers
+
 
 # Create your views here.
+
+
+class PersonaViewsets(viewsets.ModelViewSet):
+    queryset = Persona.objects.all()
+    serializers_class = PersonaSerializers
